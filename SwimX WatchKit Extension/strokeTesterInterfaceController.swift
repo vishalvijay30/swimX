bab1c0016b1c89613229048527642f491eec26cc
Anushk <anushkmittal@icloud.com>
2016-10-09 16:27:51 -0400

    added stroke tester

---
+++ SwimX WatchKit Extension/strokeTesterInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  strokeTesterInterfaceController.swift
+//  SwimX
+//
+//  Created by Anushk Mittal on 10/9/16.
+//  Copyright © 2016 Anushk Mittal. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class strokeTesterInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
